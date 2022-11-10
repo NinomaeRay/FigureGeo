9aa4964f49ab50da3863ef89666e0813f0b9366d
RayShino <rrayquazazombie@gmail.com>
2022-11-10 12:25:19 +0100

    Modificato quadrato patato

--- Quadrato.java
+++ Quadrato.java
@@ -1,3 +1,36 @@
-public class Quadrato extends Rettangolo {
+public class Quadrato {
+    private double _lato;
+
+    public Quadrato(double lato) {
+        this._lato = lato;
+    }
+
+    public double getLato() {
+        return _lato;
+    }
+
+    public void setLato(double lato) {
+        this._lato = lato;
+    }
+
+    public double getArea() {
+        return getLato() * getLato();
+    }
+
+    public double getPerimetro() {
+        return 4 * getLato();
+    }
+
+    public String toString() {
+        return "Quadrato di lato " + getLato();
+    }
+
+    public void enlarge(double factor) {
+        setLato(getLato() * factor);
+    }
+
+    public void iLoveSquares() {
+        System.out.println("I love squares!");
+    }
 }
     

2cd4b71d190edec79b73940715e791641ded0e3c
TreRote42 <68393964+TreRote42@users.noreply.github.com>
2022-11-10 10:50:33 +0100

    PrimoCommit Monta Cerchio formattato

--- Cerchio.java
+++ Cerchio.java
@@ -1,17 +1,23 @@
 public class Cerchio {
+
     private double raggio;
+
     public Cerchio(double raggio) {
         this.raggio = raggio;
     }
+
     public double getRaggio() {
         return raggio;
     }
+
     public void setRaggio(double raggio) {
         this.raggio = raggio;
     }
+
     public double getArea() {
         return Math.PI * raggio * raggio;
     }
+
     public double getPerimetro() {
         return 2 * Math.PI * raggio;
     }

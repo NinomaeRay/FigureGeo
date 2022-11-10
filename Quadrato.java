06ad69f57c1ca6be8fae22a961982e79e76de692
TreRote42 <68393964+TreRote42@users.noreply.github.com>
2022-11-10 12:24:15 +0100

    Quinto Commit Monta Update Quadrato

--- Quadrato.java
+++ Quadrato.java
@@ -1,3 +1,25 @@
 public class Quadrato extends Rettangolo {
+    public Quadrato(double lato) {
+        super(lato, lato);
+    }
+ 
+    public double getLato() {
+        return getBase();
+    }
+ 
+    public void setLato(double lato) {
+        setBase(lato);
+        setAltezza(lato);
+    }
+ 
+    public String toString() {
+        return "Quadrato di lato " + getLato();
+    }
+
+    public Rettangolo DoubleQuadrato(){
+        return new Rettangolo(getLato()*2, getLato());
+    }
+ 
 }
+
     
